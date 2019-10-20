f229adeac39e2930db24ec83c55dbe0f793ab827
Andy <andydang120@gmail.com>
2019-10-20 15:49:55 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.fooddeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
