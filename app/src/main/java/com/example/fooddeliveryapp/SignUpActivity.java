2692c52e6b7260418c10154dc297bfd37e5f2dc0
Andy <andydang120@gmail.com>
2019-10-25 23:50:33 -0400

    Added Sign-up Page

--- app/src/main/java/com/example/fooddeliveryapp/SignUpActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.fooddeliveryapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +12,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class SignUpActivity extends AppCompatActivity {
-    private Button signOut;
-    private TextView email, username;
+    private EditText name, email, street_address, city, zip_code;
+    private Button register;
+    private RadioButton deliverer, customer, restaurant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        email = findViewById(R.id.email_text);
-        username = findViewById(R.id.username_text);
-        signOut = findViewById(R.id.sign_out_button);
-
-        email.setText("Email");
-        username.setText("Username");
-        signOut.setText("Sign out");
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        street_address = findViewById(R.id.street_address);
+        city = findViewById(R.id.city);
+        zip_code = findViewById(R.id.zip_code);
+        register = findViewById(R.id.button_register);
+        deliverer = findViewById(R.id.radio_deliverer);
+        customer = findViewById(R.id.radio_customer);
+        restaurant = findViewById(R.id.radio_restaurant);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        customer.toggle();
 
         setDataOnView();
     }
@@ -38,7 +45,9 @@ public class SignUpActivity extends AppCompatActivity {
         Intent data = getIntent();
         GoogleSignInAccount gso = data.getParcelableExtra("google_account");
 
-        username.setText(gso.getDisplayName());
+        if (gso.getGivenName() != null && gso.getFamilyName() != null)
+            name.setText(gso.getGivenName() + " " + gso.getFamilyName());
+
         email.setText(gso.getEmail());
     }
 }
